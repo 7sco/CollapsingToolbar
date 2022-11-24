f6cd6c8e242a6936a6738092079924ef38a533f1
7sco <xaviandrade14@gmail.com>
2022-11-24 12:16:50 -0500

    first commit

---
+++ CollapsingToolbarLibrary/src/main/java/com/isco/collapsingtoolbarlibrary/CollapsingToolbar.kt
@@ -0,0 +1,11 @@
+package com.isco.collapsingtoolbarlibrary
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CollapsingToolbar() {
+    Box(modifier = androidx.compose.ui.Modifier.size(50.dp))
+}
